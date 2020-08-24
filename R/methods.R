7641048fc49d8e372f26eeaa35889368561318be
stemangiola <mangiolastefano@gmail.com>
2020-08-24 19:58:11 +1000

    added join_transcripts

--- R/methods.R
+++ R/methods.R
@@ -24,3 +24,93 @@ tidy <- function(object) {  UseMethod("tidy", object) }
 
 #' @export
 tidy.Seurat <- function(object){  as(object, "tidyseurat") }
+
+
+#' Add differential transcription information to a tbl using edgeR.
+#'
+#' \lifecycle{experimental}
+#'
+#' @description join_transcripts() takes as imput a `tbl` formatted as | <SAMPLE> | <TRANSCRIPT> | <COUNT> | <...> | and returns a `tbl` with additional columns for the statistics from the hypothesis test.
+#'
+#' @importFrom rlang enquo
+#' @importFrom magrittr "%>%"
+#'
+#' @name join_transcripts
+#' @rdname join_transcripts
+#'
+#' @param .data A `tbl` formatted as | <SAMPLE> | <TRANSCRIPT> | <COUNT> | <...> |
+#' @param .formula A formula with no response variable, representing the desired linear model
+#' @param .sample The name of the sample column
+#' @param .transcript The name of the transcript/gene column
+#' @param .abundance The name of the transcript/gene abundance column
+#'
+#' @param significance_threshold A real between 0 and 1 (usually 0.05).
+#' @param action A character string. Whether to join the new information to the input tbl (add), or just get the non-redundant tbl with the new information (get).
+#'
+#' @details At the moment this function uses edgeR only, but other inference algorithms will be added in the near future.
+#'
+#' @return A `tbl` with additional columns for the statistics from the hypothesis test (e.g.,  log fold change, p-value and false discovery rate).
+#'
+#'
+#'
+#'
+#' @examples
+#'\donttest{
+#'
+#'
+#'
+#' 	join_transcripts(
+#' 	    ~ condition,
+#' 	    sample,
+#' 	    transcript,
+#' 	    `count`
+#' 	)
+#'
+#'}
+#'
+#' @export
+#'
+join_transcripts <- function(.data,
+                              transcripts = NULL,
+                              all = F,
+                              exclude_zeros = F,
+                              shape = "long") {
+  UseMethod("join_transcripts", .data)
+}
+#' @export
+join_transcripts.default <-
+  function(.data,
+           transcripts = NULL,
+           all = F,
+           exclude_zeros = F,
+           shape = "long")
+  {
+    print("This function cannot be applied to this object")
+  }
+#' @export
+join_transcripts.tidyseurat <-
+  function(.data,
+           transcripts = NULL,
+           all = F,
+           exclude_zeros = F)
+  {
+    
+    message("tidyseurat says: A data frame is returned for independent data analysis.")
+
+    .data %>%
+      to_tib %>%
+      left_join(
+        get_abundance_sc_long(
+          .data = .data,
+          transcripts = transcripts,
+          all = all,
+          exclude_zeros = exclude_zeros
+        ),
+        by = "cell"
+      ) %>%
+      select(cell, transcript, contains("abundance"), everything())
+
+   
+  }
+
+
--- R/utilities.R
+++ R/utilities.R
@@ -30,3 +30,126 @@ add_class = function(var, name) {
   
   var
 }
+
+
+#' get abundance long
+#' @importFrom magrittr "%$%"
+#'
+#' @export
+get_abundance_sc_wide = function(.data, transcripts = NULL, all = F){
+  
+  
+  
+  # Check if output would be too big without forcing
+  if(
+    length(VariableFeatures(.data)) == 0  &
+    is.null(transcripts) &
+    all == F
+  ) stop("
+				 Your object do not contain variable trancript labels,
+				 transcript argument is empty and all argument is set to FALSE.
+				 Either:
+				 1. use detect_variable_features() to select variable feature
+				 2. pass an array of transcripts names
+				 3. set all = TRUE (this will output a very large object, do you computer have enough RAM?)
+				 ")
+  
+  # Get variable features if existing
+  if(
+    length(VariableFeatures(.data)) > 0  &
+    is.null(transcripts) &
+    all == F
+  ) variable_genes = VariableFeatures(.data)
+  
+  # Else
+  else variable_genes = NULL
+  
+  # Just grub last assay
+  my_assay = .data@assays %>% names  %>% tail(1)
+  
+  
+  my_assay %>%
+    when(
+      variable_genes %>% is.null %>% `!` ~ .x@counts[variable_genes,],
+      transcripts %>% is.null %>% `!` ~ .x@counts[transcripts,],
+      ~ stop("It is not convenient to extract all genes, you should have either variable features or transcript list to extract")
+    ) %>%
+    as.matrix() %>%
+    t %>%
+    as_tibble(rownames = quo_name(.transcript)) 
+  
+}
+
+#' get abundance long
+#' @importFrom magrittr "%$%"
+#' @importFrom Seurat VariableFeatures
+#' @importFrom tidyr pivot_longer
+#' @importFrom tibble as_tibble
+#' @importFrom purrr when
+#' @importFrom purrr map2
+#' 
+#'
+#' @export
+get_abundance_sc_long = function(.data, transcripts = NULL, all = F, exclude_zeros = F){
+  
+  
+  
+  # Check if output would be too big without forcing
+  if(
+    length(VariableFeatures(.data)) == 0  &
+    is.null(transcripts) &
+    all == F
+  ) stop("
+				 Your object do not contain variable trancript labels,
+				 transcript argument is empty and all argument is set to FALSE.
+				 Either:
+				 1. use detect_variable_features() to select variable feature
+				 2. pass an array of transcripts names
+				 3. set all = TRUE (this will output a very large object, do you computer have enough RAM?)
+				 ")
+  
+  
+  # Get variable features if existing
+  if(
+    length(VariableFeatures(.data)) > 0  &
+    is.null(transcripts) &
+    all == F
+  ) variable_genes = VariableFeatures(.data)
+  
+  # Else
+  else variable_genes = NULL
+  
+  assay_names = .data@assays %>% names
+  
+  
+  .data@assays %>%
+    
+    # Take active assay
+    map2(assay_names,
+         
+         ~ .x %>%
+           when(
+             variable_genes %>% is.null %>% `!` ~ .x@data[variable_genes,, drop=F],
+             transcripts %>% is.null %>% `!` ~ .x@data[ toupper(rownames(.x@data)) %in% toupper(transcripts),, drop=F],
+             all  ~ .x@data,
+             ~ stop("It is not convenient to extract all genes, you should have either variable features or transcript list to extract")
+           ) %>%
+           
+           # Replace 0 with NA
+           when(exclude_zeros ~ (.) %>% { x = (.); x[x == 0] <- NA; x }, ~ (.)) %>%
+           
+           as_tibble(rownames = "transcript") %>%
+           tidyr::pivot_longer(
+             cols = -transcript,
+             names_to ="cell", 
+             values_to = "abundance" %>% paste(.y, sep="_"),
+             values_drop_na  = TRUE
+           ) 
+         #%>%
+         #mutate_if(is.character, as.factor) %>%
+         
+         
+    ) %>%
+    Reduce(function(...) left_join(..., by=c("transcript", "cell")), .)
+  
+}
