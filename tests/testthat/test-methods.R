8256f2754638c4687e3d88b73ac1f81d53f7f7a1
Stefano Mangiola <mangiolastefano@gmail.com>
2023-06-04 15:55:21 +1000

    update method and tests

--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -13,3 +13,39 @@ test_that("join_features",{
 
 
 })
+
+test_that("aggregate_cells() returns expected values", {
+  
+  # Create pseudo-bulk object for testing
+  pbmc_pseudo_bulk <-
+    pbmc_small |>
+    aggregate_cells(c(groups, letter.idents), assays = "RNA")
+  
+  # Check row length is unchanged
+  pbmc_pseudo_bulk |>
+    distinct(.feature) |> 
+    nrow() |>
+    expect_equal(pbmc_small |> nrow())
+  
+  # Check column length is correctly modified
+  pbmc_pseudo_bulk |> 
+    distinct(.sample) |> 
+    nrow() |>
+    expect_equal(pbmc_small |>
+                   as_tibble() |>
+                   select(groups, letter.idents) |>
+                   unique() |>
+                   nrow()
+    )
+  
+  # Spot check for correctly aggregated count value of ACAP1 gene
+  pbmc_pseudo_bulk |> 
+    filter(.feature == "ACAP1" & .sample == "g1___A") |> 
+    select(RNA) |> 
+    as.numeric() |> 
+    expect_equal(Assays(pbmc_small, "RNA")["ACAP1", pbmc_small |>
+                                               as_tibble() |>
+                                               filter(groups == "g1", letter.idents == "A") |>
+                                               pull(.cell)] |>
+                   sum())
+})
