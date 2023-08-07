43ab6e91a29eb15763b1205e5a9a12699184c6b5
William Hutchison <william.8191@fastmail.com>
2023-08-07 15:45:47 +1000

    Add utility unit tests

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -34,3 +34,17 @@ test_that("get_abundance_sc_long", {
     sum() |>
     expect_equal(175.2576, tolerance = 0.1)
 })
+
+test_that("get_special_column_name_symbol", {
+  expect_equal(get_special_column_name_symbol(".cell")$symbol, rlang::sym(".cell"))
+  expect_equal(get_special_column_name_symbol(".cell")$name, c(".cell"))
+})
+
+test_that("ping_old_special_column_into_metadata", {
+  ping_old_special_column_into_metadata(pbmc_small) |>
+    as_tibble() |>
+    colnames() |>
+    purrr::pluck(1) |>
+    expect_equal("cell")
+})
+
